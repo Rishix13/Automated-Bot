bd66ef814145f4045a42813082097d01da1635d4
Rishix13 <62074767+Rishix13@users.noreply.github.com>
2020-09-21 18:57:02 +0530

    Add files via upload

---
+++ tinder_bot.py
@@ -0,0 +1,83 @@
+from selenium import webdriver
+from time import sleep
+
+#from secrets import username, password
+
+class TinderBot():
+    def __init__(self):
+        self.driver = webdriver.Chrome()
+
+    def login(self):
+        self.driver.get('https://tinder.com')
+
+        sleep(10)
+
+        #more_btn ==bot.driver.find_element_by_xpath('//*[@id="modal-manager"]/div/div/div/div/div[3]/span/button')
+        #more_btn.cick()
+
+        fb_btn  = bot.driver.find_element_by_xpath('//*[@id="modal-manager"]/div/div/div/div/div[3]/span/div[2]/button')
+        fb_btn.click()
+
+        # switch to login popup
+        base_window = self.driver.window_handles[0]
+        self.driver.switch_to_window(self.driver.window_handles[1])
+
+        email_in = self.driver.find_element_by_xpath('//*[@id="email"]')
+        email_in.send_keys("enter the email here")
+
+        pw_in = self.driver.find_element_by_xpath('//*[@id="pass"]')
+        pw_in.send_keys("enter the password here")
+
+        sleep(2)
+
+        login_btn = self.driver.find_element_by_xpath('//*[@id="u_0_0"]')
+        login_btn.click()
+
+        self.driver.switch_to_window(base_window)
+
+        popup_0 = self.driver.find_element_by_xpath('//*[@id="content"]/div/div[2]/div/div/div[1]/button')
+        popup_0.click()
+
+        sleep(5)
+
+        popup_1 = self.driver.find_element_by_xpath('//*[@id="modal-manager"]/div/div/div/div/div[3]/button[1]')
+        popup_1.click()
+
+        sleep(5)
+
+        popup_2 = self.driver.find_element_by_xpath('//*[@id="modal-manager"]/div/div/div/div/div[3]/button[1]')
+        popup_2.click()
+
+    def like(self):
+        like_btn = self.driver.find_element_by_xpath('//*[@id="content"]/div/div[1]/div/main/div[1]/div/div/div[1]/div/div[2]/div[4]/button')
+        like_btn.click()
+
+    def dislike(self):
+        dislike_btn = self.driver.find_element_by_xpath('//*[@id="content"]/div/div[1]/div/div/main/div/div[1]/div/div[2]/div[2]/button')
+        dislike_btn.click()
+
+    def auto_swipe(self):
+        while True:
+            sleep(1)
+            try:
+                self.like()
+            except Exception:
+                try:
+                    self.close_popup()
+                except Exception:
+                    self.close_match()
+                     
+
+    def close_popup(self):
+        popup_3 = self.driver.find_element_by_xpath('//*[@id="modal-manager"]/div/div/div[2]/button[2]')
+        popup_3.click()
+
+    def close_match(self):
+        match_popup = self.driver.find_element_by_xpath('//*[@id="modal-manager-canvas"]/div/div/div[1]/div/div[3]/a')
+        match_popup.click()
+
+
+
+bot = TinderBot()
+bot.login()
+bot.auto_swipe()
