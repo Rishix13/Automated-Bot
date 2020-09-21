11ed602a4f597e79150e095158e5f23233ee9aeb
Rishix13 <62074767+Rishix13@users.noreply.github.com>
2020-09-21 18:57:58 +0530

    Update tinder_bot.py

--- tinder_bot.py
+++ tinder_bot.py
@@ -1,8 +1,6 @@
 from selenium import webdriver
 from time import sleep
 
-#from secrets import username, password
-
 class TinderBot():
     def __init__(self):
         self.driver = webdriver.Chrome()
